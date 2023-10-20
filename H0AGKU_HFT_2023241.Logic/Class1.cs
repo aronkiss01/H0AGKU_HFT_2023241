e7d27bec74b9b5b2f8991f12c1bc9342e91c3406
Áron <ron@ARON>
2023-10-20 18:44:41 +0200

    Add project files.

---
+++ H0AGKU_HFT_2023241.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace H0AGKU_HFT_2023241.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ H0AGKU_HFT_2023241.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace H0AGKU_HFT_2023241.Repository
+{
+    public class Class1
+    {
+    }
+}
