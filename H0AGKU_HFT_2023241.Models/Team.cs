7a8ac46f9eabbc224f90791f448073636670d796
aron.kiss01 <aron.kiss01@stud.uni-obuda.hu>
2023-11-18 20:55:48 +0100

    ModelsUpdateV0.1

---
+++ H0AGKU_HFT_2023241.Models/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H0AGKU_HFT_2023241.Models
+{
+    public class Team
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int LeagueID { get; set; }
+
+        public bool HasJuniorSquad { get; set; }
+
+        public Team()
+        {
+            
+        }
+        public Team(int id, string name, int leagueid, bool hasjuniorsquad)
+        {
+            ID = id;
+            Name = name;
+            LeagueID = leagueid;
+            HasJuniorSquad = hasjuniorsquad;
+            
+        }
+    }
+}
