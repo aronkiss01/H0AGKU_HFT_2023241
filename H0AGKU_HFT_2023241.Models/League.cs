df7392463872f42db6e31c245abe00a573a58484
aron.kiss01 <aron.kiss01@stud.uni-obuda.hu>
2023-11-18 18:58:44 +0100

    ModelsUpdate

---
+++ H0AGKU_HFT_2023241.Models/League.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace H0AGKU_HFT_2023241.Models
+{
+    public class League
+    {
+        public int Id { get; set; }
+        public string LeagueName { get; set; }
+        public string Country { get; set; }
+        public bool HasVar { get; set; }
+
+        public League()
+        {
+            
+        }
+        public League(int id, string leagueName, string country, bool hasVar)
+        {
+            Id = id;
+            LeagueName = leagueName;
+            Country = country;
+            HasVar = hasVar;
+        }
+
+    }
+    public class JuniorLeagueInfo
+    {
+        public int LeagueId { get; set; }
+        public int JuniorSquadsInLeague { get; set; }
+
+        public JuniorLeagueInfo() { }
+
+        public JuniorLeagueInfo(int leagueId, int juniorSquadsInLeague)
+        {
+            LeagueId = leagueId;
+            JuniorSquadsInLeague = juniorSquadsInLeague;
+        }
+
+
+    }
+}
---
+++ H0AGKU_HFT_2023241.Models/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H0AGKU_HFT_2023241.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ID { get; set; }
+        public string Position { get; set; }
+        public int PlayerSalary { get; set; }
+        public bool FieldPStick { get; set; }
+
+        public Player()
+        {
+            
+        }
+        public Player(string name, int age, int iD, string position, int playerSalary, bool fieldPStick)
+        {
+            Name = name;
+            Age = age;
+            ID = iD;
+            Position = position;
+            PlayerSalary = playerSalary;
+            FieldPStick = fieldPStick;
+        }
+    }
+}
