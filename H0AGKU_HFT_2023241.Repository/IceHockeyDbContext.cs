e2571fb76e2fc304c4fb6b73c97c3a7f1a293939
aron.kiss01 <aron.kiss01@stud.uni-obuda.hu>
2023-11-20 11:35:29 +0100

    DatabaseUpdate

---
+++ H0AGKU_HFT_2023241.Repository/IceHockeyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using H0AGKU_HFT_2023241.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace H0AGKU_HFT_2023241.Repository
+{
+    public class IceHockeyDbContext:DbContext 
+    {
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player > Players { get; set; }
+
+        public IceHockeyDbContext()
+        {
+            this.Database.EnsureCreated();      
+        }
+    }
+}
