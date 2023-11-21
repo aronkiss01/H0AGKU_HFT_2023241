16f17a6a77a542abbe7bf469c7b98f0bcac29ba8
aron.kiss01 <aron.kiss01@stud.uni-obuda.hu>
2023-11-21 15:41:38 +0100

    DatabaseUpdateV0.4

--- H0AGKU_HFT_2023241.Repository/IceHockeyDbContext.cs
+++ H0AGKU_HFT_2023241.Repository/IceHockeyDbContext.cs
@@ -42,6 +42,32 @@ namespace H0AGKU_HFT_2023241.Repository
                 new League(){Id=3,LeagueName="DEL Bundesliga",Country="Germany",HasVar=true}
             };
             modelBuilder.Entity<League>().HasData(leagues);
+
+            var teams = new List<Team>()
+            {
+                new Team(){ID=1,Name="Pittsburgh Penguins",LeagueID=1,HasJuniorSquad=true},
+                new Team(){ID=2,Name="New York Rangers",LeagueID=1,HasJuniorSquad=true},
+                new Team(){ID=3,Name="Detroit Red Wings",LeagueID=1,HasJuniorSquad=false},
+                new Team(){ID=4,Name="Dunaújvárosi Acélbikák",LeagueID=2,HasJuniorSquad=true},
+                new Team(){ID=5,Name="Budapest Jégkorong Akadémia HC",LeagueID=2,HasJuniorSquad=true},
+                new Team(){ID=6,Name="DVTK Jegesmedvék",LeagueID=2,HasJuniorSquad=false},
+                new Team(){ID=7,Name="Ice Tigers Nürnberg",LeagueID=3,HasJuniorSquad=true},
+                new Team(){ID=8,Name="Eisbären Berlin",LeagueID=3,HasJuniorSquad=false},
+                new Team(){ID=9,Name="EHC Red Bull München",LeagueID=3,HasJuniorSquad=true}
+            };
+            modelBuilder.Entity<Team>().HasData(teams);
+
+            var players = new List<Player>()
+            {
+                new Player(){Name="Sidney Crosby",Id=1,Age=36,PlayerSalary=3400000,Position="Midfilder",TeamID=1},
+                new Player(){Name="Jonathan Toews",Id=2,Age=35,PlayerSalary=2900000,Position="Midfilder",TeamID=1},
+                new Player(){Name="Gilbert Brulé",Id=3,Age=32,PlayerSalary=2750000,Position="GoalTender",TeamID=1},
+                new Player(){Name="Nico Sturm",Id=4,Age=28,PlayerSalary=1600000,Position="Midfilder",TeamID=2},
+                new Player(){Name="Hári János",Id=5,Age=31,PlayerSalary=1970000,Position="Midfilder",TeamID=2},
+                new Player(){Name="Arany Gergely",Id=6,Age=26,PlayerSalary=1500000,Position="GoalTender",TeamID=2},
+            };
+            modelBuilder.Entity<Player>().HasData(players);
+            
         }
 
     }
