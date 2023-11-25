164bed20b49289248b1a1644ada84a7765099959
aron.kiss01 <aron.kiss01@stud.uni-obuda.hu>
2023-11-25 21:34:03 +0100

    EndpointUpdate

--- H0AGKU_HFT_2023241.Endpoint/Startup.cs
+++ H0AGKU_HFT_2023241.Endpoint/Startup.cs
@@ -1,8 +1,13 @@
+using H0AGKU_HFT_2023241.Logic;
+using H0AGKU_HFT_2023241.Models;
+using H0AGKU_HFT_2023241.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +19,27 @@ namespace H0AGKU_HFT_2023241.Endpoint
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IceHockeyDbContext>();
+            services.AddTransient<IRepository<Player>, PlayerRepository>();
+            services.AddTransient<IRepository<Team>, TeamRepository>();
+            services.AddTransient<IRepository<League>, LeagueRepository>();
+
+            services.AddTransient<IPlayerLogic, PlayerLogic>();
+            services.AddTransient<ITeamLogic, TeamLogic>();
+            services.AddTransient<ILeagueLogic, LeagueLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "H0AGKU_HFT_2023241.Endpoint", Version = "v1" }); });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,17 +48,14 @@ namespace H0AGKU_HFT_2023241.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "H0AGKU_HFT_2023241.Endpoint"));
             }
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+           app.UseAuthentication();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
