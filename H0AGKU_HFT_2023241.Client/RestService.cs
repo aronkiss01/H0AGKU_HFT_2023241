a78aa28050afa007d113eaed869f990adce3b0d8
aron.kiss01 <aron.kiss01@stud.uni-obuda.hu>
2023-11-26 12:24:21 +0100

    ClientUpdate

---
+++ H0AGKU_HFT_2023241.Client/RestService.cs
@@ -0,0 +1,83 @@
+﻿using H0AGKU_HFT_2023241.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H0AGKU_HFT_2023241.Client
+{
+    public class RestException
+    {
+        public RestException() { }
+        public string ExceptionMsg { get; set; }
+    }
+}
+public class RestService
+{
+    HttpClient client;
+
+    public RestService(string url, string pingEndpoint = "swagger")
+    {
+        bool isConnected = false;
+        do
+        {
+            isConnected = Ping(url + pingEndpoint);
+
+        } while (isConnected.Equals(false));
+        Init(url);
+    }
+    public bool Ping(string url)
+    {
+        try
+        {
+            WebClient wclient = new WebClient();
+            wclient.DownloadData(url);
+            return true;
+        }
+        catch
+        {
+
+            return false;
+        }
+    }
+    public void Init(string Url)
+    {
+        client = new HttpClient();
+        client.BaseAddress = new Uri(Url);
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        try
+        {
+            client.GetAsync("").GetAwaiter().GetResult();
+        }
+        catch (HttpRequestException)
+        {
+
+            throw new ArgumentException("The Endpoint is not available!");
+        }
+    }
+    public List<T> Get<T>(string endP)
+    {
+        List<T> items = new List<T>();
+        HttpResponseMessage response = client.GetAsync(endP).GetAwaiter().GetResult();
+        if (response.IsSuccessStatusCode)
+        {
+            items = response.Content.ReadAsAsync<List<T>>().GetAwaiter().GetResult();
+        }
+        else
+        {
+            var error = response.Content.ReadAsAsync<RestException>().GetAwaiter().GetResult();
+            throw new ArgumentException(error.ExceptionMsg);
+
+
+
+        }
+        return items;
+    }
+
+}
+
