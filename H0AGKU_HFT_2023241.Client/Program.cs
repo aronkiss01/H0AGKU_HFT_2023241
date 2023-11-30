68ff6dd8e5764e5eae01074be4901e7649ab99be
aron.kiss01 <aron.kiss01@stud.uni-obuda.hu>
2023-11-30 17:20:09 +0100

    ConsoleMenu

--- H0AGKU_HFT_2023241.Client/Program.cs
+++ H0AGKU_HFT_2023241.Client/Program.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using H0AGKU_HFT_2023241.Models;
+using System;
 
 namespace H0AGKU_HFT_2023241.Client
 {
     internal class Program
     {
+        static RestService RestService;
+
+        static void Create(string entity)
+        {
+            if (entity == "League")
+            {
+                Console.Write("League name: ");
+                string Lname = Console.ReadLine();
+                Console.Write("League country: ");
+                string Lcountry = Console.ReadLine();
+                Console.Write("League has VAR? (true/false): ");
+                bool hasVar = bool.Parse(Console.ReadLine());
+                RestService.Post(new League() {LeagueName = Lname, Country = Lcountry, HasVar = hasVar }, "league");
+
+            }
+            else if (entity == "Player")
+            {
+                Console.Write("Player name: ");
+                string Plname = Console.ReadLine();
+                Console.Write("Player age: ");
+                int PLage = int.Parse(Console.ReadLine());
+                Console.Write("Player position: ");
+                string Ppos = Console.ReadLine();
+                Console.Write("Is player right footed?(true/false): ");
+                bool FStick = bool.Parse(Console.ReadLine());
+                Console.Write("Player salary: ");
+                int Psalary = int.Parse(Console.ReadLine());
+                Console.Write("Player team ID: ");
+                int TeamId = int.Parse(Console.ReadLine());
+                RestService.Post(new Player() {Name=Plname,Age=PLage,Position=Ppos,FieldPStick=FStick,PlayerSalary=Psalary,TeamID=TeamId },"player");
+            }
+            else if (entity == "Team")
+            {
+                Console.Write("Team name: ");
+                string Tname = Console.ReadLine();
+                Console.Write("Team league ID: ");
+                int TLId = int.Parse(Console.ReadLine());
+                Console.Write("Team has youth squad?(true/false): ");
+                bool juniorSquad = bool.Parse(Console.ReadLine());
+                RestService.Post(new Team() { Name = Tname, LeagueID = TLId, HasJuniorSquad = juniorSquad }, "team");
+            }
+            Console.ReadLine();
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
